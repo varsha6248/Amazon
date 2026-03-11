2155ca96d3153f555155a3414f0cfc20c6d8aa36
varsha6248 <varsha31072005@gmail.com>
2026-03-11 21:29:38 +0530

    Updated deployed frontend URLs and routing

--- Backend/server.js
+++ Backend/server.js
@@ -15,14 +15,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static("public"));
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB error:", err));
 
 app.use("/api", ProductRoutes);
-
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
